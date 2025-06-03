4ca5a251e7e0cba3fe819a2723e875c1d5f1c9ac
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-06-03 16:23:04 +0530

    done

--- recursion.cpp
+++ recursion.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
 
+//Print factorial of any number using recursion  --->>
+// int fact (int a) {
+//     if (a==1 || a==0){
+//         return 1;                         //base case
+//     }
+
+//     int ans = a * fact(a-1);               //recursive call      
+//     return ans;
+// }
+
+// int main() {
+//     int a;
+//     cout<<"enter no.: ";
+//     cin>>a;
+//     cout<<"Result: "<<fact(a);
+
+
 //Print Table of any number using recursion  --->>
 // void table(int n, int i = 1){
 //     if(i>10) {
@@ -21,21 +38,21 @@ using namespace std;
 
 
 //Print Power of any number using recursion  --->>
-double power (double base, int exponent){
-    if(exponent == 0) {
-        return 1; 
-    }
+// double power (double base, int exponent){
+//     if(exponent == 0 || base == 1) {
+//         return 1;                                 //base condition
+//     }
 
-    return base * power(base, exponent-1);
-}
+//     return base * power(base, exponent-1);         //recusrsive call
+// }
 
-int main(){
-    double base ;
-    cout<<"enter base: ";
-    cin>>base;
-    int exponent;
-    cout<<"enter exponent: ";
-    cin>>exponent;
-    cout<< "Result: " <<power(base,exponent);
+// int main(){
+//     double base ;
+//     cout<<"enter base: ";
+//     cin>>base;
+//     int exponent;
+//     cout<<"enter exponent: ";
+//     cin>>exponent;
+//     cout<< "Result: " <<power(base,exponent);
     return 0;
 }
