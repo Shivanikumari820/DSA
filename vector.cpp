479307b0e2d6fec771f2a6dd74c6d3a04c61b988
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-07-01 22:25:46 +0530

    done

---
+++ vector.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v;
+int main() {
+   int s;
+   cout<<"enter size";
+   cin>>s;
+    for(int i=0; i<s;i++) {
+      cin>>v[i];
+
+      if(v[i] < i+1) {
+        
+      }
+    }
+   
+
+}
