ca3eadcfe6d8508848bb3f0a4666c88cfdc4c778
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-07-03 22:50:04 +0530

    done

--- String_.cpp/String_Ques.cpp
+++ String_.cpp/String_Ques.cpp
@@ -10,14 +10,31 @@
 //     return 0;
 // }
 
-#include <bits/stdc++.h>
+
+
+//convert Cybrom into  cYBROM
+#include <iostream>
 using namespace std;
 
 int main() {
-    string s = "cybrom";
-    for (int i = 0; i < s.size(); i++) {
-        s[i] = s.at(i) + 1;                       // Using .at() to access each character
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++) {
+        // If character is lowercase (a to z)
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] = s[i] - ('a' - 'A');  // convert to uppercase
+        }
+        // If character is uppercase (A to Z)
+        else if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] = s[i] + ('a' - 'A');  // convert to lowercase
+        }
+        // Other characters are left unchanged
     }
-    cout << s;  
+
+    cout << "Converted string: " << s << endl;
+
     return 0;
 }
+
