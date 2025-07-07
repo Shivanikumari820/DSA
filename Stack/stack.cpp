daeabdf3f8af0e8f8af291eb00abf7d7a3df0ce5
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-07-07 16:46:18 +0530

    done

---
+++ Stack/stack.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class cybrom 
+{
+    public:int *arr;
+    int n, top;
+    cybrom (int a){
+        n = a;
+        arr = new int[n];
+        top = -1;
+    }
+    void push(int v){
+        if(top == n-1){
+            cout<<"\n Overflow \n";
+            return ;
+        }
+        top ++;
+        arr[top] = v;
+    }
+    int tops(){
+        if(top == -1){
+            cout<< "\n Underflow \n";
+            return -1;
+        }
+        return arr[top];
+    }
+    void pop(){
+        if(top == -1){
+            cout<<"\n There is no data \n";
+            return ;
+        }
+        top --;
+    }
+};
+
+int main() {
+    int s ; 
+    cout<<"Enter size of array \n";
+    cin>>s;
+    cybrom c(s);
+    c.push(12);
+    c.push(22);
+    c.push(32);
+    c.push(42);
+
+    while(c.top != -1){
+        cout<<"\n The data in stack is \n";
+        cout<<c.tops()<<"\n";
+        c.pop();
+    }
+    // cout<<c.tops();
+    // c.pop();
+    // c.pop();
+    // c.pop();
+    // cout<<"\n top = "<<c.tops();
+
+}
