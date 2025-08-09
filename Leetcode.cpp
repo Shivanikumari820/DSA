04a86962749efef4ec9d5693054a6f5aa2da6f40
Shivanikumari820 <shivanikumari4218@gmail.com>
2025-08-09 20:40:15 +0530

    done

--- Leetcode.cpp
+++ Leetcode.cpp
@@ -722,4 +722,26 @@ set bits
 //     return 0;
 // }
 
- 
+//  Ques no. 258 
+
+// #include <iostream>
+// using namespace std;
+
+// int addDigits(int num) {
+//     while (num >= 10) {
+//         int sum = 0;
+//         while (num > 0) {
+//             sum += num % 10;
+//             num /= 10;
+//         }
+//         num = sum;
+//     }
+//     return num;
+// }
+
+// int main() {
+//     int num;
+//     cin >> num;
+//     cout << addDigits(num);
+//     return 0;
+// }
